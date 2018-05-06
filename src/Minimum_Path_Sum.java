d8277eb4ce46b8b71022be876c63cc4a00a69dc5
xjwhhh <151250171@smail.nju.edu.cn>
2018-05-06 16:28:33 +0800

    5.6

---
+++ src/Minimum_Path_Sum.java
@@ -0,0 +1,34 @@
+public class Minimum_Path_Sum {
+    //两个方法的区别是是否构造了新的数组
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i=1;i<n;i++){
+            grid[0][i] += grid[0][i-1];
+        }
+        for(int i=1;i<m;i++){
+            grid[i][0] += grid[i-1][0];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+
+    public int minPathSum1(int[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for(int i = 1;i<grid[0].length;i++)
+            dp[0][i] += dp[0][i-1] + grid[0][i];
+        for(int i = 1;i<grid.length;i++)
+            dp[i][0] += dp[i-1][0] + grid[i][0];
+        for(int i = 1;i<m;i++)
+            for(int j = 1;j<n;j++)
+                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+        return dp[m-1][n-1];
+    }
+}
---
+++ src/Unique_Pahts_II.java
@@ -0,0 +1,44 @@
+public class Unique_Pahts_II {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int width = obstacleGrid[0].length;
+        int[] dp = new int[width];
+        dp[0] = 1;
+        for (int[] row : obstacleGrid) {
+            for (int j = 0; j < width; j++) {
+                if (row[j] == 1)
+                    dp[j] = 0;
+                else if (j > 0)
+                    dp[j] += dp[j - 1];
+            }
+        }
+        return dp[width - 1];
+    }
+
+
+    public int uniquePathsWithObstacles1(int[][] obstacleGrid) {
+
+        //Empty case
+        if(obstacleGrid.length == 0) return 0;
+
+        int rows = obstacleGrid.length;
+        int cols = obstacleGrid[0].length;
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(obstacleGrid[i][j] == 1)
+                    obstacleGrid[i][j] = 0;
+                else if(i == 0 && j == 0)
+                    obstacleGrid[i][j] = 1;
+                else if(i == 0)
+                    obstacleGrid[i][j] = obstacleGrid[i][j - 1] * 1;// For row 0, if there are no paths to left cell, then its 0,else 1
+                else if(j == 0)
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] * 1;// For col 0, if there are no paths to upper cell, then its 0,else 1
+                else
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+            }
+        }
+
+        return obstacleGrid[rows - 1][cols - 1];
+
+    }
+}
