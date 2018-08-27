4c91d7ebf5269bac86593394d06bbec4558836ad
xjw <xjwhhh233@outlook.com>
2018-08-27 16:08:55 +0800

    8.27

---
+++ src/Flatten_Binary_Tree_to_Linked_List.java
@@ -0,0 +1,12 @@
+public class Flatten_Binary_Tree_to_Linked_List {
+    private TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
